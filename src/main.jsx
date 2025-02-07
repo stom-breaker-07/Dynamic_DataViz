0a73cb1fad34a09ec08019d2d65ff0ca095d6aec
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-02-07 12:49:10 +0530

    Fixed routing issue with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from "react-router-dom"; // Change to HashRouter
+import { BrowserRouter } from "react-router-dom"; // Keep BrowserRouter
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/Dynamic_DataViz/"> {/* Ensure basename is set */}
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
